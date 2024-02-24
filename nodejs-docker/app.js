9c322c7250c109daefded69c2a2ef99adbe21c96
Vaibhav Singhal <vaibhavsinghal87@gmail.com>
2024-02-24 15:47:58 +0000

    db connection

--- nodejs-docker/app.js
+++ nodejs-docker/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var db = require('./database/database');
+db.connect();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- nodejs-docker/database/database.js
+++ nodejs-docker/database/database.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 // db connection
-try {
-    mongoose.connect('mongodb+srv://admin:admin@cluster0.mqbayp8.mongodb.net/node-app').then(() => {
-      console.log('Mongo connected');
-    });
-}
-catch (error) {
-    console.log(error);
-    process.exit(1);
-}
+const connect = async () => { 
+    try {
+        mongoose.connect('mongodb+srv://admin:admin@cluster0.mqbayp8.mongodb.net/node-app').then(() => {
+        console.log('Mongo connected');
+        });
+    }
+    catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connect;
